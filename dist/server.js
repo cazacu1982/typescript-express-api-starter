cb1952b4580ac120461ded86f375106ef739be6e
drca@netop.com <drca@netop.com>
2019-07-31 10:07:44 +0300

    initial commit

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(5000, () => console.log('Example app listening on port 5000!'));
+//# sourceMappingURL=server.js.map
